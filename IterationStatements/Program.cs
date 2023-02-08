e1597093613e361ecbdf55326f3933b11484e17c
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-08 10:23:01 -0500

    iteration exercise

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,18 +1,77 @@
-﻿namespace IterationStatements
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace IterationStatements
 {
     public class Program
     {
         //LukeWarm Section: Create methods below
         //Write a method that will print to the console all numbers 1000 through - 1000
+        static void PrintNumbers()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+
 
         //Write a method that will print to the console numbers 3 through 999 by 3 each time
-
+        public static void Print999()
+        {
+            for (int i = 3; i <= 999; i+= 3)
+            {
+            
+                Console.WriteLine(i);
+               
+            }
+        }
         //Write a method to accept two integers as parameterss and check whether they are equal or not
-        
+        public static void Equal(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+
+
+
         //Write a method to check whether a given number is even or odd
-        
+        public static void EvenorOdd(int num1)
+        {
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Even");
+
+            }
+
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+
         //Write a method to check whether a given number is positive or negative
-        
+        public static void PositiveOrNegative(int num1)
+        {
+            if (num1 > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
+        }
+           
         //Write a method to read the age of a candidate and determine whether they can vote.
         //Hint: Use Parse or the safer TryParse() for an extra challenge
         //Parse()
@@ -20,14 +79,31 @@
 
         //Heatin Up Section:
         //Write a method to check if an integer(from the user) is in the range -10 to 10
-        
+        public static bool TenRange(int num)
+        {
+            //Console.WriteLine("type in a number");
+            //var num = int.Parse(Console.ReadLine());
+            if (num <=10&& num>=-10)
+            {
+                Console.WriteLine("this number is in range");
+                return true;
+                
+            }
+            else
+            {
+                Console.WriteLine("this number is not in range");
+                return false;   
+
+            }
+        }   
         //Write a method to display the multiplication table(from 1 to 12) of a given integer
 
 
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+          //TenRange(4);
+            PrintNumbers();
         }
     }
 }
